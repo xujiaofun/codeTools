82d19c59de5db41523658b1440bb05e4d4a44adf
jax <361246278@qq.com>
2018-01-25 15:40:19 +0800

    xxx

--- 切图小工具/CutImage.py
+++ 切图小工具/CutImage.py
@@ -1,10 +1,13 @@
 # -*- coding:utf-8 -*-
 import os
+import math
 from PIL import Image
 
-def splitimage(src, rownum, colnum, dstpath):
+def splitimage(src, colwidth, rowheight, dstpath):
     img = Image.open(src)
     w, h = img.size
+    rownum = math.ceil(h / rowheight)
+    colnum = math.ceil(w / colwidth)
     if rownum <= h and colnum <= w:
         print('Original image info: %sx%s, %s, %s' % (w, h, img.format, img.mode))
         print('开始处理图片切割, 请稍候...')
@@ -17,12 +20,10 @@ def splitimage(src, rownum, colnum, dstpath):
         ext = fn[-1]
 
         num = 0
-        rowheight = h // rownum
-        colwidth = w // colnum
         for r in range(rownum):
             for c in range(colnum):
-                box = (c * colwidth, r * rowheight, (c + 1) * colwidth, (r + 1) * rowheight)
-                p = dstpath + "/" + basename + '_' + str(num) + '.' + ext
+                box = (c * colwidth, h - (r+1) * rowheight, (c + 1) * colwidth, h - (r) * rowheight)
+                p = dstpath + "/" + basename + '_' + str(r + 1) + '_' + str(c + 1) + '.' + ext
                 img.crop(box).save(p, "jpeg")
                 num = num + 1
 
@@ -30,16 +31,11 @@ def splitimage(src, rownum, colnum, dstpath):
     else:
         print('不合法的行列切割参数！')
 
-src = raw_input('input path:')
+src = input('input path:')
 if os.path.isfile(src):
-    dstpath = raw_input('请输入图片输出目录（不输入路径则表示使用源图片所在目录）：')
+    dstpath = input('请输入图片输出目录（不输入路径则表示使用源图片所在目录）：')
     if (dstpath == '') or os.path.exists(dstpath):
-        row = int(input('请输入切割行数：'))
-        col = int(input('请输入切割列数：'))
-        if row > 0 and col > 0:
-            splitimage(src, row, col, dstpath)
-        else:
-            print('无效的行列切割参数！')
+        splitimage(src, 512, 512, dstpath)
     else:
         print('图片输出目录 %s 不存在！' % dstpath)
 else:
